b0ec5a55a29cf58a50a460231c4472b507ce3675
Yuanyuan Gan <yuanyuangan@YuanyuandeMacBook-Pro.local>
2019-05-14 19:40:34 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+// import {NgsRevealConfig} from 'ngx-scrollreveal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // providers: [NgsRevealConfig]
 })
 export class AppComponent {
   title = 'myweb';
+
+  // constructor(config: NgsRevealConfig) {
+  //
+  //   config.duration = 3000;
+  //   config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
+  //
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+import {NgsRevealModule} from 'ngx-scrollreveal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgsRevealModule
   ],
   providers: [],
   bootstrap: [AppComponent]
